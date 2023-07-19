53076cdff765f4bba4ab9fbc04f032b502e9ea13
algisbernatovics <algis.bernatovics@gmail.com>
2023-07-19 14:26:21 +0300

    Bug fixes.

--- rock-paper-scissors-ext/public/RockPaperScissorsGame.php
+++ rock-paper-scissors-ext/public/RockPaperScissorsGame.php
@@ -6,95 +6,114 @@ require_once '../app/Models/GameElement.php';
 class RockPaperScissorsGame
 {
     protected array $gameElements;
-    protected Player $user;
-    protected array $computerPlayers;
+    protected array $computerUsers;
+    protected array $computerBots;
     protected const ROUNDS = 3;
 
-    public function __construct(Player $user, array $computerPlayers, array $gameElements)
+    public function __construct(array $computerUsers, array $computerBots, array $gameElements)
     {
         $this->gameElements = $gameElements;
-        $this->user = $user;
-        $this->computerPlayers = $computerPlayers;
+        $this->computerUsers = $computerUsers;
+        $this->computerBots = $computerBots;
     }
 
     public function start(): void
     {
-        foreach ($this->computerPlayers as $player) {
-            for ($round = 1; $round <= self::ROUNDS; $round++) {
-                echo "Round: $round with {$player->getName()}" . PHP_EOL;
-                $userChoice = $this->getUserChoice();
-                $player->setChoice($this->getComputerChoice());
-                $this->playRound($this->user, $player, $userChoice);
+        foreach ($this->computerUsers as $user) {
+            foreach ($this->computerBots as $bot) {
+                $this->playRounds($user, $bot);
+                $this->determineWinner($user, $bot);
             }
-            $this->determineWinner($this->user, $player);
         }
         $this->printResults();
     }
 
-    public function playRound(Player $user, Player $opponent, int $userChoice): void
+    public function playRounds(Player $user, Player $bot): void
     {
-        $userElement = $this->gameElements[$userChoice];
-        $computerElement = $this->gameElements[$opponent->getChoice()];
+        for ($round = 1; $round <= self::ROUNDS; $round++) {
+            echo "Round: $round with {$bot->getName()}" . PHP_EOL;
+            $this->setUserChoice($user);
+            $this->setBotChoice($bot);
 
-        echo "{$user->getName()} chooses {$userElement->getName()}" . PHP_EOL;
-        echo "{$opponent->getName()} chooses {$computerElement->getName()}" . PHP_EOL;
+            $userElement = $this->gameElements[$user->getChoice()];
+            $computerElement = $this->gameElements[$bot->getChoice()];
 
-        if ($userElement->beats($computerElement)) {
-            echo "{$user->getName()} wins!" . PHP_EOL;
-            $user->increasePoints();
-        } elseif ($computerElement->beats($userElement)) {
-            echo "{$opponent->getName()} wins!" . PHP_EOL;
-            $opponent->increasePoints();
-        } else {
-            echo "It's a draw!" . PHP_EOL;
+            echo "{$user->getName()} chooses {$userElement->getName()}" . PHP_EOL;
+            echo "{$bot->getName()} chooses {$computerElement->getName()}" . PHP_EOL;
+
+            if ($userElement->beats($computerElement)) {
+                echo "{$user->getName()} wins!" . PHP_EOL;
+                $user->increasePoints();
+            } elseif ($computerElement->beats($userElement)) {
+                echo "{$bot->getName()} wins!" . PHP_EOL;
+                $bot->increasePoints();
+            } else {
+                echo "It's a draw!" . PHP_EOL;
+            }
+            echo PHP_EOL;
         }
-        echo PHP_EOL;
     }
 
-    private function getUserChoice(): int
+    private function setUserChoice(Player $user)
     {
         $gameElementsCount = count($this->gameElements);
-        $choice = readline("{$this->user->getName()}, enter choice (1-$gameElementsCount): ");
+        $this->printGameElements();
+        $choice = readline("{$user->getName()}, enter choice (1-$gameElementsCount): ");
         $range = range(1, $gameElementsCount);
 
         while (!in_array($choice, $range)) {
             echo "Invalid choice. Please enter a number between 1 and $gameElementsCount." . PHP_EOL;
-            $choice = readline("{$this->user->getName()}, enter choice (1-$gameElementsCount): ");
+            $this->printGameElements();
+            $choice = readline("{$user->getName()}, enter choice (1-$gameElementsCount): ");
         }
-        return intval($choice);
+        $user->setChoice($choice);
     }
 
-    public function getComputerChoice(): int
+    public function setBotChoice(Player $bot)
     {
-        return rand(1, count($this->gameElements));
+        $bot->setChoice(rand(1, count($this->gameElements)));
     }
 
-    public function determineWinner(Player $user, Player $opponent): void
+    public function printGameElements()
+    {
+        foreach ($this->gameElements as $key => $element) {
+            echo $key . ':' . $element->getName() . '|';
+        }
+        echo PHP_EOL;
+    }
+
+    public function determineWinner(Player $user, Player $bot): void
     {
         if ($user->getPoints() >= 2) {
             $user->increaseWins();
-        } elseif ($opponent->getPoints() >= 2) {
-            $opponent->increaseWins();
+        } elseif ($bot->getPoints() >= 2) {
+            $bot->increaseWins();
         }
         $user->resetPoints();
-        $opponent->resetPoints();
+        $bot->resetPoints();
     }
 
     private function printResults(): void
     {
-        echo "{$this->user->getName()} has {$this->user->getWins()} win(s)." . PHP_EOL;
-        foreach ($this->computerPlayers as $player) {
-            echo "{$player->getName()} has {$player->getWins()} win(s)." . PHP_EOL;
+        foreach ($this->computerBots as $bot) {
+            echo "{$bot->getName()} has {$bot->getWins()} win(s)." . PHP_EOL;
+        }
+        echo PHP_EOL;
+        foreach ($this->computerUsers as $user) {
+            echo "{$user->getName()} has {$user->getWins()} win(s)." . PHP_EOL;
         }
         echo PHP_EOL;
     }
 }
 
-$computerUser = new Player('Captain Copy Paste');
+$computerUsers = [
+    new Player('Captain Copy Paste'),
+];
 
-$computerPlayers = [
+$computerBots = [
     new Player('Facepalm (Bot)'),
     new Player('Master of Dynamite (Bot)'),
+    new Player('Papercraft Ninja (Bot)'),
 ];
 
 $gameElements = [
@@ -106,4 +125,4 @@ $gameElements = [
     6 => new GameElement('Lizard', $beats = ['Paper', 'Spock'])
 ];
 
-$game = (new RockPaperScissorsGame($computerUser, $computerPlayers, $gameElements))->start();
+$game = (new RockPaperScissorsGame($computerUsers, $computerBots, $gameElements))->start();
